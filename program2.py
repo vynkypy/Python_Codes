3eb022258967ec1e1303e95d85f1eedf7bd9d72f
vynkypy <vynkatesh.pandit@gmail.com>
2024-11-08 00:09:41 +0530

    Substraction of two numbers

---
+++ program2.py
@@ -0,0 +1,8 @@
+# substraction of two numbers
+
+a = int(input('Enter First the number: '))
+b = int(input('Enter Second the number: '))
+
+Ans = a - b
+
+print('Addition of given numbers is: ', Ans)
