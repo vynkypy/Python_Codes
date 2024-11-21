7f186d0b46907f085f2eb64b6765ec9e351abfca
vynkypy <vynkatesh.pandit@gmail.com>
2024-11-21 15:10:24 +0530

    Python Program to Find Area of a Circle

---
+++ program8.py
@@ -0,0 +1,12 @@
+# Python Program to Find Area of a Circle
+
+# Area = pi * r2
+# where r is radius of circle 
+
+radius = int(input("Enter the radius of circle: "))
+
+pi = 3.142
+area = pi * (radius*radius)
+
+print(f"Area of circle with {radius} radius is: ", area)
+
