d6b1a49498626f430545d7c1f35e288f1689e866
vynkypy <vynkatesh.pandit@gmail.com>
2025-01-14 18:45:20 +0530

    program30

---
+++ program30.py
@@ -0,0 +1,13 @@
+# Problem Statement: Accept two numbers from user and perform it's addition
+
+
+def addition(no1, no2):
+    ans = no1+no2
+    return ans
+
+
+i = int(input("Enter the first number: "))
+j = int(input("Enter the second number: "))
+
+
+print('addition of given numbers is: ', addition(i, j))
